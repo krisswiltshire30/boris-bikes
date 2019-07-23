9e4e2ed36ec51191f474d46259d0414f3cb5b0e3
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-07-23 19:12:11 +0100

    Second test complete - A

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,11 @@
 class Bike
-  def working?
+
+  def initialize
+    @working = true
   end
+
+  def working?
+    @working
+  end
+
 end
--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,12 @@
+require_relative 'bike'
+
 class DockingStation
 
-  def release_bike
+  def initialize
   end
+
+  def release_bike
+    Bike.new
+  end
+
 end
