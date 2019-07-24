fc285ef7bbc43f47ebdb5a9691be7829dddc6d10
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-07-24 14:24:09 +0100

    Updated RSpec tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-require 'docking_station'
-require 'bike'
+require './lib/docking_station'
+require './lib/bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+  bike = subject.release_bike
+  expect(bike).to be_working
+end
+
 end
